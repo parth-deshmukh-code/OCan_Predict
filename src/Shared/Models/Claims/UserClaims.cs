7951dd513c4d2f5efd99b58f763897e5ac334908
MrDave1999 <daveseva2010@hotmail.es>
2024-03-30 20:16:38 -0500

    refactor: Initialize default value to properties
    This prevents NullReferenceException.

--- src/Shared/Models/Claims/UserClaims.cs
+++ src/Shared/Models/Claims/UserClaims.cs
@@ -4,7 +4,7 @@ public class UserClaims
 {
     public int UserId { get; init; }
     public int PersonId { get; init; }
-    public string UserName { get; init; }
-    public string FullName { get; init; }
-    public IEnumerable<string> Roles { get; init; }
+    public string UserName { get; init; } = string.Empty;
+    public string FullName { get; init; } = string.Empty;
+    public IEnumerable<string> Roles { get; init; } = [];
 }
