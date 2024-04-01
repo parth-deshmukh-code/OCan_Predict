841e11b4160edb172ffa86d0f5c6d3dd26e0e949
MrDave1999 <daveseva2010@hotmail.es>
2024-04-01 13:04:07 -0500

    style: Add XML comment

--- src/Shared/Interfaces/IDateTimeService.cs
+++ src/Shared/Interfaces/IDateTimeService.cs
@@ -1,5 +1,8 @@
 ﻿namespace DentallApp.Shared.Interfaces;
 
+/// <summary>
+/// This interface allows tests to run successfully regardless of the system clock.
+/// </summary>
 public interface IDateTimeService
 {
     DateTime Now { get; }
