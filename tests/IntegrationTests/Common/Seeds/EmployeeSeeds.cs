31b903c09194d9d970ee004e77d69087118b6c14
MrDave1999 <daveseva2010@hotmail.es>
2024-04-06 19:41:20 -0500

    Create new seed data for testing

---
+++ tests/IntegrationTests/Common/Seeds/EmployeeSeeds.cs
@@ -0,0 +1,43 @@
+﻿namespace IntegrationTests.Common.Seeds;
+
+public class EmployeeSeeds
+{
+    public static List<Employee> Get()
+        =>
+        [
+            new()
+            {
+                Id = 1,
+                UserId = 3,
+                PersonId = 3,
+                OfficeId = 1
+            },
+            new()
+            {
+                Id = 2,
+                UserId = 4,
+                PersonId = 4,
+                OfficeId = 1,
+                PregradeUniversity = "UG",
+                PostgradeUniversity = "UG"
+            },
+            new()
+            {
+                Id = 3,
+                UserId = 5,
+                PersonId = 5,
+                OfficeId = 1,
+                PregradeUniversity = "UG",
+                PostgradeUniversity = "UG"
+            },
+            new()
+            {
+                Id = 4,
+                UserId = 6,
+                PersonId = 6,
+                OfficeId = 1,
+                PregradeUniversity = "UG",
+                PostgradeUniversity = "UG"
+            }
+        ];
+}
