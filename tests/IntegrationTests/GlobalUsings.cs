47656deff95dd6ed3b8b9812285ab2db2c836640
MrDave1999 <daveseva2010@hotmail.es>
2024-04-08 13:01:30 -0500

    chore: Update global usings

--- tests/IntegrationTests/GlobalUsings.cs
+++ tests/IntegrationTests/GlobalUsings.cs
@@ -11,6 +11,7 @@ global using MySqlConnector;
 global using Respawn;
 
 global using DentallApp.Infrastructure;
+global using DentallApp.Infrastructure.Services;
 global using DentallApp.Infrastructure.Persistence;
 global using DentallApp.Shared.Interfaces;
 global using DentallApp.Shared.Constants;
@@ -22,6 +23,7 @@ global using DentallApp.Shared.Models.Claims;
 
 global using DentallApp.Core.Persons.UseCases;
 global using DentallApp.Core.Dependents.UseCases;
+global using DentallApp.Core.Security.Users.UseCases;
 
 global using IntegrationTests.Common;
 global using IntegrationTests.Common.Seeds;
