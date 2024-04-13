bdb672ae9e9b517daca6be1c1b0f8a70ea69fec8
MrDave1999 <daveseva2010@hotmail.es>
2024-04-13 12:53:24 -0500

    test: Add unit tests for CurrentUserService type

---
+++ tests/UnitTests/Infrastructure/Services/CurrentUserServiceTests.cs
@@ -0,0 +1,85 @@
+﻿namespace UnitTests.Infrastructure.Services;
+
+public class CurrentUserServiceTests
+{
+    [Test]
+    public void PersonId_WhenClaimHasCorrectFormat_ShouldReturnsPersonId()
+    {
+        // Arrange
+        int expectedPersonId = 1;
+        var claims = new List<Claim> 
+        { 
+            new(CustomClaimsType.PersonId, "1") 
+        };
+        var claimsIdentity = new ClaimsIdentity(claims);
+        var claimsPrincipal = new ClaimsPrincipal(claimsIdentity);
+        var currentUser = new CurrentUserService(claimsPrincipal);
+
+        // Act
+        int actual = currentUser.PersonId;
+
+        // Assert
+        actual.Should().Be(expectedPersonId);
+    }
+
+    [Test]
+    public void UserId_WhenClaimHasCorrectFormat_ShouldReturnsUserId()
+    {
+        // Arrange
+        int expectedUserId = 1;
+        var claims = new List<Claim>
+        {
+            new(CustomClaimsType.UserId, "1")
+        };
+        var claimsIdentity = new ClaimsIdentity(claims);
+        var claimsPrincipal = new ClaimsPrincipal(claimsIdentity);
+        var currentUser = new CurrentUserService(claimsPrincipal);
+
+        // Act
+        int actual = currentUser.UserId;
+
+        // Assert
+        actual.Should().Be(expectedUserId);
+    }
+
+    [Test]
+    public void UserName_WhenClaimHasCorrectFormat_ShouldReturnsUserName()
+    {
+        // Arrange
+        string expectedUserName = "Bob";
+        var claims = new List<Claim>
+        {
+            new(CustomClaimsType.UserName, "Bob")
+        };
+        var claimsIdentity = new ClaimsIdentity(claims);
+        var claimsPrincipal = new ClaimsPrincipal(claimsIdentity);
+        var currentUser = new CurrentUserService(claimsPrincipal);
+
+        // Act
+        string actual = currentUser.UserName;
+
+        // Assert
+        actual.Should().Be(expectedUserName);
+    }
+
+    [TestCaseSource(nameof(Actions))]
+    public void User_WhenClaimNotFound_ShouldThrowClaimNotFoundException(Action act)
+    {
+        // Act & Assert
+        act.Should().Throw<ClaimNotFoundException>();
+    }
+
+    static IEnumerable<Action> Actions()
+    {
+        var claims = new List<Claim>
+        {
+            new("NotFound", "1")
+        };
+        var claimsIdentity = new ClaimsIdentity(claims);
+        var claimsPrincipal = new ClaimsPrincipal(claimsIdentity);
+        var currentUser = new CurrentUserService(claimsPrincipal);
+        yield return () => { int personId = currentUser.PersonId; };
+        yield return () => { int userId = currentUser.UserId; };
+        yield return () => { string userName = currentUser.UserName; };
+    }
+}
