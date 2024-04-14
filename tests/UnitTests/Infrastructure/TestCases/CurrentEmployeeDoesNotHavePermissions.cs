4b91ecb8fbe1170448150fb70907b80aa8066946
MrDave1999 <daveseva2010@hotmail.es>
2024-04-14 08:52:55 -0500

    test: Create separate classes for test cases
    This prevents the test file called CurrentEmployeeServiceTests.cs from being inflated.

---
+++ tests/UnitTests/Infrastructure/TestCases/CurrentEmployeeDoesNotHavePermissions.cs
@@ -0,0 +1,120 @@
+﻿namespace UnitTests.Infrastructure.TestCases;
+
+public class CurrentEmployeeDoesNotHavePermissions : IEnumerable
+{
+    public IEnumerator GetEnumerator()
+    {
+        yield return new object[]
+        {
+            RoleName.Admin,
+            new List<int>
+            {
+                (int)Role.Predefined.Unverified,
+                (int)Role.Predefined.BasicUser,
+                (int)Role.Predefined.Admin,
+                (int)Role.Predefined.Superadmin
+            }
+        };
+
+        yield return new object[]
+        {
+            RoleName.Admin,
+            new List<int> { (int)Role.Predefined.Unverified }
+        };
+
+        yield return new object[]
+        {
+            RoleName.Admin,
+            new List<int> { (int)Role.Predefined.BasicUser }
+        };
+
+        yield return new object[]
+        {
+            RoleName.Admin,
+            new List<int> { (int)Role.Predefined.Admin }
+        };
+
+        yield return new object[]
+        {
+            RoleName.Admin,
+            new List<int> { (int)Role.Predefined.Superadmin }
+        };
+
+        yield return new object[]
+        {
+            RoleName.Superadmin,
+            new List<int> { (int)Role.Predefined.Unverified }
+        };
+
+        yield return new object[]
+        {
+            RoleName.Superadmin,
+            new List<int> { (int)Role.Predefined.BasicUser }
+        };
+
+        yield return new object[]
+        {
+            RoleName.Superadmin,
+            new List<int> { (int)Role.Predefined.Superadmin }
+        };
+
+        yield return new object[]
+        {
+            RoleName.Superadmin,
+            new List<int>
+            {
+                (int)Role.Predefined.Unverified,
+                (int)Role.Predefined.BasicUser,
+                (int)Role.Predefined.Superadmin
+            }
+        };
+
+        yield return new object[]
+        {
+            RoleName.Secretary,
+            new List<int> { (int)Role.Predefined.Secretary }
+        };
+
+        yield return new object[]
+        {
+            RoleName.Secretary,
+            new List<int> { (int)Role.Predefined.Dentist }
+        };
+
+        yield return new object[]
+        {
+            RoleName.Secretary,
+            new List<int> { (int)Role.Predefined.Admin }
+        };
+
+        yield return new object[]
+        {
+            RoleName.Secretary,
+            new List<int> { (int)Role.Predefined.Superadmin }
+        };
+
+        yield return new object[]
+        {
+            RoleName.Dentist,
+            new List<int> { (int)Role.Predefined.Secretary }
+        };
+
+        yield return new object[]
+        {
+            RoleName.Dentist,
+            new List<int> { (int)Role.Predefined.Dentist }
+        };
+
+        yield return new object[]
+        {
+            RoleName.Dentist,
+            new List<int> { (int)Role.Predefined.Admin }
+        };
+
+        yield return new object[]
+        {
+            RoleName.Dentist,
+            new List<int> { (int)Role.Predefined.Superadmin }
+        };
+    }
+}
