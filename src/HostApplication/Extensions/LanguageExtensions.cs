6d7b35e5364287963b39ce3b33ce12a9d60a12df
MrDave1999 <daveseva2010@hotmail.es>
2024-07-01 11:51:07 -0500

    refactor: Add methods such as GetLanguages and GetDefaultLanguage

--- src/HostApplication/Extensions/LanguageExtensions.cs
+++ src/HostApplication/Extensions/LanguageExtensions.cs
@@ -3,24 +3,29 @@
 public static class LanguageExtensions
 {
     public static IServiceCollection ConfigureLanguages(
-        this IServiceCollection services, 
+        this IServiceCollection services,
         IConfiguration configuration)
     {
         services.Configure<RequestLocalizationOptions>(options =>
         {
-            var languages = configuration
-                .GetRequiredSection("Languages")
-                .Get<string[]>() ?? [];
-
             List<CultureInfo> supportedCultures = [];
-            foreach(var language in languages)
+            var languages = configuration.GetLanguages();
+            foreach (var language in languages)
                 supportedCultures.Add(new CultureInfo(language));
 
-            var defaultLanguage = configuration.GetValue<string>("DefaultLanguage");
+            var defaultLanguage = configuration.GetDefaultLanguage();
             options.DefaultRequestCulture = new RequestCulture(defaultLanguage);
             options.SupportedCultures = supportedCultures;
             options.SupportedUICultures = supportedCultures;
         });
         return services;
     }
+
+    public static string[] GetLanguages(this IConfiguration configuration)
+        => configuration
+            .GetRequiredSection("Languages")
+            .Get<string[]>() ?? [];
+
+    public static string GetDefaultLanguage(this IConfiguration configuration)
+        => configuration.GetValue<string>("DefaultLanguage") ?? "es";
 }
