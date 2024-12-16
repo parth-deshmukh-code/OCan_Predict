0451acfb8681e25a0a9dac4ae5c02c55359eec72
parth-deshmukh-code <deshmukhparth002@gmail.com>
2024-12-16 12:20:08 +0530

    Delete tests directory

--- tests/IntegrationTests/Common/TestSettings.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace IntegrationTests.Common;
-
-public class TestSettings
-{
-    public const string CurrentDateTime = "CURRENT_DATETIME";
-    public const string BaseUri = "/api/";
-}
--- tests/UnitTests/Shared/Entities/EmployeeSchedules/EmployeeScheduleFake.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace UnitTests.Shared.Entities.EmployeeSchedules;
-
-public class EmployeeScheduleFake : IEmployeeSchedule
-{
-    public TimeSpan? MorningStartHour { get; set; }
-    public TimeSpan? MorningEndHour { get; set; }
-    public TimeSpan? AfternoonStartHour { get; set; }
-    public TimeSpan? AfternoonEndHour { get; set; }
-}
